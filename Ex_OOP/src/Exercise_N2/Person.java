0b7ccdf5fb1858ab563a7cf160a729d8566e2531
Roman <roman_dra@mail.ru>
2022-06-29 17:06:35 +0500

    add matrix check

--- Ex_OOP/src/Exercise_N2/Person.java
+++ Ex_OOP/src/Exercise_N2/Person.java
@@ -4,7 +4,7 @@ public class Person {
 
     private String fullName;
     private int age;
-    
+
     public Person(String fullName, int age) {
 
         this.fullName = fullName;
--- Ex_OOP/src/Exercize_N3/Matrix.java
+++ Ex_OOP/src/Exercize_N3/Matrix.java
@@ -8,14 +8,18 @@ public class Matrix {
 
     public int[][] sumMatrix(int[][] otherMatrix) {
         int[][] result = new int[this.row][this.col];
-        for (int i = 0; i < this.matrix.length; i++) {
-            for (int j = 0; j < this.matrix[i].length; j++) {
-                result[i][j] = this.matrix[i][j] + otherMatrix[i][j];
+
+        if(result.length == this.matrix.length && result[0].length == this.matrix[0].length) {
+            for (int i = 0; i < this.matrix.length; i++) {
+                for (int j = 0; j < this.matrix[i].length; j++) {
+                    result[i][j] = this.matrix[i][j] + otherMatrix[i][j];
+                }
             }
-
+        } else {
+            System.out.println("Нельзя складывать матрицы разных размеров");
         }
-        return result;
 
+        return result;
     }
 
     public int[][] numMultMatrix(int num) {
@@ -39,12 +43,16 @@ public class Matrix {
 
     public int[][] multMatrix(int[][] otherMatrix) {
         int[][] result = new int[this.matrix.length][otherMatrix[0].length];
-        for (int i = 0; i < result[0].length; i++) {
-            for (int j = 0; j < result.length; j++) {
-                for (int k = 0; k < this.matrix[0].length; k++) {
-                    result[i][j] = result[i][j] + this.matrix[i][k] * otherMatrix[k][j];
+        if(result[0].length  == this.matrix.length) {
+            for (int i = 0; i < result[0].length; i++) {
+                for (int j = 0; j < result.length; j++) {
+                    for (int k = 0; k < this.matrix[0].length; k++) {
+                        result[i][j] = result[i][j] + this.matrix[i][k] * otherMatrix[k][j];
+                    }
                 }
             }
+        } else {
+            System.out.println("Матрицы не согласованы");
         }
         return result;
     }
