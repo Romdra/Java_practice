e051a4a6e48c697d5858c835459cabcaf41e34cb
Roman <roman_dra@mail.ru>
2022-07-02 21:55:49 +0500

    upd

--- Ex_OOP/src/Exercise_N6/com/company/details/Engine.java
+++ Ex_OOP/src/Exercise_N6/com/company/details/Engine.java
@@ -2,9 +2,15 @@ package Exercise_N6.com.company.details;
 
 public class Engine {
 
-    private int power;
+    private String power;
     private String company;
 
-    public int getPower() { return this.power; }
+    public Engine(String power, String company) {
+
+        this.power = power;
+        this.company = company;
+    }
+
+    public String getPower() { return this.power; }
     public String getCompany() { return this.company; }
 }
